4d806054d1caf70f9508ee6d73385fdb27c4a40b
NoumanSabir86 <35195045+NoumanSabir86@users.noreply.github.com>
2020-08-12 15:58:40 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
 
 router.get("/favicon.ico", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
 
 module.exports = router;
 
